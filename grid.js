88a311a6ffcd275830bc7f22cfe5dab47b2a6a1c
mbukhman <mbukhman@gmail.com>
2022-11-17 01:46:39 +0400

    Add eventListener, Auto-Grid in JS

--- grid.js
+++ grid.js
@@ -1,24 +1,28 @@
 function buildGrid(gridSize, parent) {
-  const cellWidth = Math.ceil(screen.width / gridSize);
-  const cellHeight = Math.ceil(screen.height / gridSize);
+  const cellWidth = Math.floor(screen.width / gridSize);
+  const cellHeight = Math.floor(screen.height / gridSize);
   const cellSize = cellWidth > cellHeight ? cellHeight : cellWidth;
 
-  const newDiv = document.createElement("div", {});
-  newDiv.style.width = cellSize + "px";
-  newDiv.style.height = cellSize + "px";
-  newDiv.id = "grid";
-  
+  const newGrid = document.createElement("div");
+  newGrid.id = "grid";
+  newGrid.style.cssText = `grid-template-columns: repeat(${gridSize}, 1fr);`;
 
-  for (let i = 0; i < gridSize*gridSize; i++) {
-      newDiv.cloneNode();
-    }
-    
-    const gridOldDiv = document.querySelector("#grid");
-    if (gridOldDiv) parent.removeChile(gridOldDiv);
+  for (let i = 0; i < gridSize * gridSize; i++) {
+    let newDiv = document.createElement("div");
+    newDiv.style.width = cellSize + "px";
+    newDiv.style.height = cellSize + "px";
+    newDiv.id = `cell${i}`;
+    newGrid.appendChild(newDiv);
+  }
 
-    parent.appendChild(newDiv);
+  const oldGrid = document.querySelector("#grid");
+  if (oldGrid) parent.removeChild(oldGrid);
+
+  parent.appendChild(newGrid);
 }
 
-const gridSize = 16;
+const gridSize = 10;
 const content = document.querySelector("#content");
 buildGrid(gridSize, content);
+
+window.addEventListener("resize", (event) => buildGrid(gridSize, content));
