7cfd2e720fd0fa572014753826bf8c772a2e5ccf
mbukhman <mbukhman@gmail.com>
2022-11-17 02:49:00 +0400

    Add mouseover events and animation

--- grid.js
+++ grid.js
@@ -8,15 +8,29 @@ function buildGrid(gridSize, parent) {
   newGrid.style.cssText = `grid-template-columns: repeat(${gridSize}, 1fr);`;
 
   for (let i = 0; i < gridSize * gridSize; i++) {
-    let newDiv = document.createElement("div");
-    newDiv.style.width = cellSize + "px";
-    newDiv.style.height = cellSize + "px";
-    newDiv.id = `cell${i}`;
-    newGrid.appendChild(newDiv);
+    let newCell = document.createElement("div");
+    newCell.style.width = cellSize + "px";
+    newCell.style.height = cellSize + "px";
+    newCell.id = `cell${i}`;
+    newCell.addEventListener("mouseover", (e) => {
+      e.target.classList.remove("off");
+      e.target.classList.add("on");
+    });
+    newCell.addEventListener("mouseout", (e) => {
+      e.target.classList.remove("on");
+      e.target.classList.add("off");
+    });
+    newGrid.appendChild(newCell);
   }
 
   const oldGrid = document.querySelector("#grid");
   if (oldGrid) parent.removeChild(oldGrid);
+
+  newGrid.childNodes.forEach((element) => {
+    element.addEventListener("mouseover", (event) => {
+      event.target.classList.add("on");
+    });
+  });
 
   parent.appendChild(newGrid);
 }
